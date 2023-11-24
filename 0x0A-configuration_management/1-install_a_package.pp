d9654de9b4c41cb5b629d38bc02502f4dc978af8
jessicasewe <jessicasewe89@gmail.com>
2023-11-24 08:11:24 -0800

    installing flask and werkzeug

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# installs the package  puppet-lint
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.2.2',
+  provider => 'pip3',
+}
