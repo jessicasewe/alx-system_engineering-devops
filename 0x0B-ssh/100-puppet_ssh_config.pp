a3fcd3c8707d41472a2e2c255fc41e230fe43ee5
jessicasewe <jessicasewe89@gmail.com>
2023-11-26 09:27:08 -0800

    tests

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,14 @@
 # set up your client SSH configuration file so that you can connect to a server without typing a password.
-file_line {'Turn off passwd auth'
+file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+  line   => 'PasswordAuthentication no',
+  match  => 'PasswordAuthentication',
 }
 
-file_line {'Declare Identity file'
+file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/school',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => 'IdentityFile',
 }
