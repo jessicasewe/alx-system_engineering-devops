97f126fa632906a634e1e6a608ef348f6084ba11
jessicasewe <jessicasewe89@gmail.com>
2024-01-04 00:15:21 -0800

    corrections made for gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-""" Uses REST API to return information about an employee """
+"""Returns to-do list information for a given employee ID."""
+import json
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python3 module_name.py ID")
-        sys.exit(1)
+    id = sys.argv[1]
 
-    ID = int(sys.argv[1])
-    if not isinstance(ID, int):
-        print("ID must be an integer")
-        sys.exit(1)
+    user = requests.get(f"https://jsonplaceholder.typicode.com/users/?id={id}")
+    todos = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos/?userId={id}"
+    )
 
-    url = "https://jsonplaceholder.typicode.com/"
-    employee_data = requests.get(url + "users/{}".format(ID)).json()
-    todos = requests.get(url + "todos", params={"userId": ID}).json()
+    user = json.loads(user.text)
+    todos = json.loads(todos.text)
 
-    completed_tasks = [todo.get("title") for todo
-                       in todos if todo.get("completed") is True]
-    total_tasks = len(todos)
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = len(todos)
 
-    print("Employee {} is done with tasks({}/{}):".format(
-        (employee_data.get("name")), len(completed_tasks), total_tasks))
-    [print("\t {}".format(task)) for task in completed_tasks]
+    for todo in todos:
+        if todo.get("completed"):
+            NUMBER_OF_DONE_TASKS += 1
+
+    print(f"Employee {user[0].get('name')} is done "
+          f"with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    for todo in todos:
+        if todo.get("completed"):
+            print(f"\t {todo.get('title')}")
