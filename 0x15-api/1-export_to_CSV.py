f23186fa53de5be894478ea2f5aa2ca15739c5c2
jessicasewe <jessicasewe89@gmail.com>
2024-01-04 00:18:51 -0800

    corrections for export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python3
-""" Uses REST API to return information about an employee
-    Exports employee records to a csv file
-"""
+"""Exports a csv with a to-do list information for a given employee ID."""
 import csv
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python3 module_name.py ID")
-        sys.exit(1)
+    id = sys.argv[1]
 
-    ID = int(sys.argv[1])
-    if not isinstance(ID, int):
-        print("ID must be an integer")
-        sys.exit(1)
+    user = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/?id={id}"
+    ).json()[0]
+    todos = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos/?userId={id}"
+    ).json()
 
-    url = "https://jsonplaceholder.typicode.com/"
-    employee_data = requests.get(url + "users/{}".format(ID)).json()
-    todos = requests.get(url + "todos", params={"userId": ID}).json()
+    with open(f"{id}.csv", "w", newline="") as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
 
-    userName = employee_data.get("username")
-    task_status = [task.get("completed") for task in todos]
-    task_title = [title.get("title") for title in todos]
-
-    rows = zip([ID] * len(task_title), [userName] * len(task_title),
-               task_status, task_title)
-
-    filename = f"{ID}.csv"
-    with open(filename, mode="w", newline="") as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        writer.writerows(rows)
+        for todo in todos:
+            writer.writerow([id,
+                             user.get("username"),
+                             todo.get("completed"),
+                             todo.get("title")])
