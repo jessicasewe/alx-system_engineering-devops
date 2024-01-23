453a653a91effa007e13aaaf1aa5c2cf4a5b0af7
jessicasewe <jessicasewe89@gmail.com>
2024-01-23 10:50:13 -0800

    increasing the amount of traffic an nginx server can handle

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+#increase the amount of traffic an nginx server can handle
+
+#increase the ulimit of the  default file
+
+exec { 'file--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin'
+} ->
+
+#restart nginx
+exec {'nginx-restart':
+  command => 'nginx-restart',
+  path    => '/etc/init.d/'
+}
