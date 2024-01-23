12ecf1a289f1816a1381a35ba0dffd7f57f7d8d8
jessicasewe <jessicasewe89@gmail.com>
2024-01-23 11:23:36 -0800

    correction on client configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
 # set up your client SSH configuration file so that you can connect to a server without typing a password.
+file { 'etc/ssh/ssh_config':
+  ensure => present,
+}
+
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
