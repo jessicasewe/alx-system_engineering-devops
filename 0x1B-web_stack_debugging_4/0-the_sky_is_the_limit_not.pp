64c5296ab666df7829f32c58717fdfb745954f7d
jessicasewe <jessicasewe89@gmail.com>
2024-01-26 01:20:58 -0800

    arrow moved

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,10 +5,10 @@
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin'
-} ->
+}
 
 #restart nginx
-exec {'nginx-restart':
+-> exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
